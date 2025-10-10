263c6cccde53138670fcddbad5478475b0e11bff
Ditzzy <143317954+cv3inx@users.noreply.github.com>
2025-10-10 17:53:50 +0700

    Update tail.sh

--- tail.sh
+++ tail.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --auth-key=tskey-auth-kAjFM3aKjG11CNTRL-Kscb2mpjpqXRxv4ZgYzoqXANLw5EeYiR7
+curl -fsSL https://tailscale.com/install.sh | sh && sudo tailscale up --ssh --auth-key=tskey-auth-kAjFM3aKjG11CNTRL-Kscb2mpjpqXRxv4ZgYzoqXANLw5EeYiR7
